38e741d1cffe57ea1681095470ed40c94b296e99
rpenela <rpenela@persiscalconsulting.com>
2021-06-04 02:06:48 -0300

    contacto terminado

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,49 @@
 import React from 'react'
 import Puntitos from './random/Puntitos'
 import Subtitulo from './random/Subtitulo'
-
+import "./contact.css"
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import emailjs from 'emailjs-com';
+import logoMail from '../assets/mails.svg'
+import Swal from 'sweetalert2';
+
 
 const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
+  name: Yup.string()
+    .min(2, 'Muy corto')
+    .max(50, 'Muy largo!')
+    .required('Campo obligatorio'),
+  email: Yup.string()
+    .email('Mail invalido')
+    .required('Campo obligatorio'),
+  message: Yup.string()
+    .min(2, 'Muy corto')
+    .max(200, 'Muy largo!')
+    .required('Campo obligatorio'),
 });
 
 export default function Contact(){
+
+    function sendEmail(e) {
+        e.preventDefault();
+        if(e.target.name.value !== "" && e.target.email.value !== "" && e.target.message.value){
+            emailjs.sendForm('service_ttsy6uy', 'template_t4fsev6', e.target, 'user_K893iL7VdE8hE59qWHaTD')
+            .then((result) => {
+                e.target.reset()
+                console.log(result.text);
+                Swal.fire(
+                    'Mail enviado!',
+                    'Te estare contestando pronto ;)',
+                    'success'
+                    )
+                }, (error) => {
+                    console.log(error.text);
+                });
+        }
+      }
+
+
     return(
         <div className="container">
             <div className="d-flex container justify-content-start pb-4">
@@ -25,36 +51,36 @@ export default function Contact(){
                 <Subtitulo subTitulo={"Contact"}/>
             </div>
 
-            <div>
-                <div>
+            <div className="row">
+                <div className="col-12 col-lg-6 px-4 d-flex flex-column justify-content-center align-items-center mb-5">
                     <h3>Si deseas contactarme lo podes hacer a traves del formulario.</h3>
+                    <img style={{width: "360px"}} src={logoMail} alt="logo de contactos"/>
                 </div>
-                <div>
+
+                <div className="col-12 col-lg-6 px-4 pt-lg-5">
                 <Formik
                     initialValues={{
-                        firstName: '',
-                        lastName: '',
+                        name: '',
                         email: '',
+                        message: '',
                     }}
                     validationSchema={SignupSchema}
                     onSubmit={values => {
-                        // same shape as initial values
+                        
                         console.log(values);
                     }}
                     >
                     {({ errors, touched }) => (
-                        <Form>
-                        <Field name="firstName" />
-                        {errors.firstName && touched.firstName ? (
-                            <div>{errors.firstName}</div>
-                        ) : null}
-                        <Field name="lastName" />
-                        {errors.lastName && touched.lastName ? (
-                            <div>{errors.lastName}</div>
-                        ) : null}
-                        <Field name="email" type="email" />
-                        {errors.email && touched.email ? <div>{errors.email}</div> : null}
-                        <button type="submit">Submit</button>
+                        <Form onSubmit={sendEmail} className="d-flex flex-column justify-content-center">
+                            <Field className="inputFormik" name="name" placeholder="Nombre..." />
+                            {errors.name? (
+                                <div className="alerta" style={{height:"28px"}}>{errors.name}</div>
+                            ) : <div style={{height:"28px"}}></div>}
+                            <Field className="inputFormik" name="email" type="email" placeholder="Ingrese mail..." />
+                            {errors.email ? <div className="alerta" style={{height:"28px"}}>{errors.email}</div> : <div style={{height:"28px"}}></div>}
+                            <Field className="inputFormik" style={{resize: "none"}} name="message" component="textarea" placeholder="Ingresar comentario..." />
+                            {errors.message ? <div className="alerta" style={{height:"28px"}}>{errors.message}</div> : <div style={{height:"28px"}}></div>}
+                            <button className="btn-submit mt-2" type="submit">Enviar</button>
                         </Form>
                     )}
                     </Formik>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,27 @@ import "./navbar.css"
 
 function Navbar() {
     return(
-        <nav className="navegacion">
-            <div>
-                <h1>Rodri</h1>
-            </div>
-            <ul>
-                <li>
-                <a href="">About</a>
-                </li>
-                <li>
-                <a href="">Skills</a>
-                </li>
-                <li>
-                <a href="">Projects</a>
-                </li>
-                <li>
-                <a href="">Contact</a>
-                </li>
-            </ul>
-        </nav>
+        <div className="d-flex justify-content-center">
+            <nav className="navegacion row">
+                <div className="col-12 col-md-6">
+                    <h1>Rodrigo Penela</h1>
+                </div>
+                <ul className="col-12 col-md-6">
+                    <li>
+                    <a href="">About</a>
+                    </li>
+                    <li>
+                    <a href="">Skills</a>
+                    </li>
+                    <li>
+                    <a href="">Projects</a>
+                    </li>
+                    <li>
+                    <a href="">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
