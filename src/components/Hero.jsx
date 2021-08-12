162dbf5df77afdcdddcc4d3baa9cfd37ce53a58f
rpenela <rpenela@persiscalconsulting.com>
2021-08-11 22:52:58 -0300

    refactor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,17 @@ import "./hero.css"
 function Hero() {
     return(
       <div className="hero">
-        <img src={profile} alt="profile"/>
-        <h1>Hi, i´m Rodri!</h1>
-        <p>Full Stack Developer</p>
-        <a href="https://www.linkedin.com/in/rodrigomanuelpenela" target="_blank">
-        <i class="linkedin fab fa-linkedin"></i>
-        </a>
-        <a className="botonMain" href="#about">About me</a>
+        <div class="h-100 w-100 capa-negra d-flex justify-content-center align-items-center">
+          <div class="caja-blanca shadow px-4 pb-4 pt-2 d-flex flex-column align-items-center">
+            <img src={profile} alt="profile"/>
+            <h1>Hi, i´m Rodri!</h1>
+            <p>Full Stack Developer</p>
+            <a href="https://www.linkedin.com/in/rodrigomanuelpenela" target="_blank">
+              <i class="linkedin fab fa-linkedin"></i>
+            </a>
+            <a className="botonMain" href="#about">About me</a>
+          </div>
+        </div>
       </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,4 +27,40 @@ function Navbar() {
     )
 }
 
-export default Navbar;
+function NavbarNew(){
+    return(
+        <div id="home" class="container">
+            <nav class="navbar navbar-expand-lg navbar-light w-100">
+                <div class="container">
+                    <a class="navbar-brand" href="#"><span>{"<"}</span>Rodri Penela<span>{"/>"}</span></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <div class="d-flex justify-content-start justify-content-lg-end w-100">
+                            <ul class="navbar-nav">
+                                <li class="nav-item mx-2">
+                                <a class="nav-link" href="#">About</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                <a class="nav-link" href="#">Skills</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                <a class="nav-link" href="#">Services</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                <a class="nav-link" href="#">Projects</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                <a class="nav-link" href="#">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavbarNew;
